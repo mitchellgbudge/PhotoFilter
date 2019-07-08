1b54016f98b82992d27f0ebb8d0b6c57e6a408ad
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-08 11:58:13 -0600

    Add filter methods to sliders

--- PhotoViewController.swift
+++ PhotoViewController.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class PhotoViewController: UIViewController {
 
-    // MARK: - Actions
+    // MARK: - Methods & Actions
+    
+    func updateImage() {
+        if let originalImage = originalImage {
+            imageView.image = image(byFiltering: originalImage)
+        } else {
+            imageView.image = nil
+        }
+    }
+    
+    private func image(byFiltering image: UIImage) -> UIImage {
+        guard let cgImage = originalImage?.cgImage else { return image }
+        let ciImage = CIImage(cgImage: cgImage)
+        filter?.setValue(ciImage, forKey: "inputImage")
+        filter?.setValue(saturationSlider.value, forKey: "inputSaturation")
+        filter?.setValue(brightnessSlider.value, forKey: "inputBrightness")
+        filter?.setValue(contrastSlider.value, forKey: "inputContrast")
+        guard let outputCIImage = filter?.outputImage else { return image }
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else { return image }
+        return UIImage(cgImage: outputCGImage)
+    }
     
     @IBAction func choosePhotoButtonPressed(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            NSLog("The photo library is unavailable")
+            return
+        }
         
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func savePhotoButtonPressed(_ sender: Any) {
@@ -21,27 +49,50 @@ class PhotoViewController: UIViewController {
     }
     
     @IBAction func brightnessValueChanged(_ sender: Any) {
-        
+        updateImage()
     }
     
     @IBAction func contrastValueChanged(_ sender: Any) {
-        
+        updateImage()
     }
     
     @IBAction func saturationValueChanged(_ sender: Any) {
-        
+        updateImage()
     }
     
     
     // MARK: - Outlets & Properties
     
+    var originalImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
+    private let filter = CIFilter(name: "CIColorControls")
+    private let context = CIContext(options: nil)
+    
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var brightnessSlider: UISlider!
-    
     @IBOutlet weak var contrastSlider: UISlider!
-    
     @IBOutlet weak var saturationSlider: UISlider!
     
 }
 
+extension PhotoViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+}
