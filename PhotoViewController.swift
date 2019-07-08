9c757bc1ab736efc9ddef5f4576c70e9a275645e
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-08 10:50:39 -0600

    Configure PhotoViewController.swift

---
+++ PhotoViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  PhotoFilter
+//
+//  Created by Mitchell Budge on 7/8/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    // MARK: - Actions
+    
+    @IBAction func choosePhotoButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func savePhotoButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func brightnessValueChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func contrastValueChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saturationValueChanged(_ sender: Any) {
+        
+    }
+    
+    
+    // MARK: - Outlets & Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var brightnessSlider: UISlider!
+    
+    @IBOutlet weak var contrastSlider: UISlider!
+    
+    @IBOutlet weak var saturationSlider: UISlider!
+    
+}
+
